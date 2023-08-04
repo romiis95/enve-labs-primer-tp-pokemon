68348c1dbc1779fd2a50bd8ab86eefe84b11de2b
romiis95 <romiirodriguez89@gmail.com>
2023-08-04 16:30:20 -0300

    trabajo de pokemones terminado

--- index.js
+++ index.js
@@ -1,316 +1,308 @@
 const pikachu = {
-    name: "Pikachu",
-    type: "electric",
-    ability: {
-      primary: "Static",
-      hidden: "Lightning rod"
-    },
-    stats: {
-      hp: 35,
-      attack: 55,
-      deffense: 40,
-      speed: 90
-    },
-    moves: [
-      "Quick Attack",
-      "Volt Tackle",
-      "Iron Tail",
-      "Thunderbolt"
-    ],
-    modifiers: {
-      weakness: ["ground"],
-      resistances: ["electric", "flying", "water", "steel"]
-    }
-  };
-  
-  const bulbasaur = {
-    name: "Bulbasaur",
-    type: "grass",
-    ability: {
-      primary: "Overgrow",
-      hidden: "Chlorophyll"
-    },
-    stats: {
-      hp: 45,
-      attack: 49,
-      deffense: 59,
-      speed: 45
-    },
-    moves: [
-      "Growl",
-      "Tackle",
-      "Vine Whip",
-      "Razor Leaf"
-    ],
-    modifiers: {
-      weakness: ["fire", "ice", "flying", "psychic"],
-      resistances: ["water", "grass", "electric", "fighter"]
-    }
-  };
-  
-  const charmander = {
-    name: "Charmander",
-    type: "fire",
-    ability: {
-      primary: "Blaze",
-      hidden: "Solar Power"
-    },
-    stats: {
-      hp: 39,
-      attack: 52,
-      deffense: 43,
-      speed: 65
-    },
-    moves: [
-      "Growl",
-      "Scratch",
-      "Flamethrower",
-      "Dragon Breath"
-    ],
-    modifiers: {
-      weakness: ["water", "ground", "rock"],
-      resistances: ["fire", "ice", "grass", "steal"]
-    }
-  };
-  
-  const squirtle = {
-    name: "Squirtle",
-    type: "water",
-    ability: {
-      primary: "Torrent",
-      hidden: "Rain Dish"
-    },
-    stats: {
-      hp: 44,
-      attack: 48,
-      deffense: 50,
-      speed: 43
-    },
-    moves: [
-      "Tackle",
-      "Tail Whip",
-      "Water Gun",
-      "Hydro Pump"
-    ],
-    modifiers: {
-      weakness: ["electric", "grass"],
-      resistances: ["water", "fire", "ice", "steel"]
-    }
-  };
-  
-  function getMoves(pokemon) {
-    return pokemon.moves;
+  name: "Pikachu",
+  type: "electric",
+  ability: {
+    primary: "Static",
+    hidden: "Lightning rod"
+  },
+  stats: {
+    hp: 35,
+    attack: 55,
+    defense: 40,
+    speed: 90
+  },
+  moves: [
+    "Quick Attack",
+    "Volt Tackle",
+    "Iron Tail",
+    "Thunderbolt"
+  ],
+  modifiers: {
+    weakness: ["ground"],
+    resistances: ["electric", "flying", "water", "steel"]
   }
-  
-  console.log("Movimientos de Pikachu:", getMoves(pikachu));
-  
-  function getPrimaryAbility(pokemon) {
-    return pokemon.ability.primary;
+};
+
+const bulbasaur = {
+  name: "Bulbasaur",
+  type: "grass",
+  ability: {
+    primary: "Overgrow",
+    hidden: "Chlorophyll"
+  },
+  stats: {
+    hp: 45,
+    attack: 49,
+    defense: 59,
+    speed: 45
+  },
+  moves: [
+    "Growl",
+    "Tackle",
+    "Vine Whip",
+    "Razor Leaf"
+  ],
+  modifiers: {
+    weakness: ["fire", "ice", "flying", "psychic"],
+    resistances: ["water", "grass", "electric", "fighter"]
   }
-  
-  console.log("Habilidad primaria de Bulbasaur:", getPrimaryAbility(bulbasaur));
-  
-  function getWeaknesses(pokemon) {
-    return pokemon.modifiers.weakness;
+};
+
+const charmander = {
+  name: "Charmander",
+  type: "fire",
+  ability: {
+    primary: "Blaze",
+    hidden: "Solar Power"
+  },
+  stats: {
+    hp: 39,
+    attack: 52,
+    defense: 43,
+    speed: 65
+  },
+  moves: [
+    "Growl",
+    "Scratch",
+    "Flamethrower",
+    "Dragon Breath"
+  ],
+  modifiers: {
+    weakness: ["water", "ground", "rock"],
+    resistances: ["fire", "ice", "grass", "steel"]
   }
-  
-  console.log("Tipos contra los que es débil Charmander:", getWeaknesses(charmander));
-  
-  function resistsType(pokemon, type) {
-    const resistances = pokemon.modifiers.resistances;
-    return resistances.includes(type);
+};
+
+const squirtle = {
+  name: "Squirtle",
+  type: "water",
+  ability: {
+    primary: "Torrent",
+    hidden: "Rain Dish"
+  },
+  stats: {
+    hp: 44,
+    attack: 48,
+    defense: 50,
+    speed: 43
+  },
+  moves: [
+    "Tackle",
+    "Tail Whip",
+    "Water Gun",
+    "Hydro Pump"
+  ],
+  modifiers: {
+    weakness: ["electric", "grass"],
+    resistances: ["water", "fire", "ice", "steel"]
   }
-  
-  console.log("¿Charmander es resistente al tipo 'electric'?", resistsType(charmander, "electric"));
-  console.log("¿Charmander es resistente al tipo 'fire'?", resistsType(charmander, "fire"));
-  
-  function resistsMove(pokemon, move) {
-    const resistances = pokemon.modifiers.resistances;
-    return resistances.includes(move.type);
-  }
-  
-  const rainDanceMove = {
-    name: "Rain dance",
-    type: "water"
-  };
-  const flamethrowerMove = {
-    name: "flamethower",
-    type: "fire"
-  };
-  
-  console.log("¿Pikachu es resistente al movimiento 'Rain dance'?", resistsMove(pikachu, rainDanceMove));
-  console.log("¿Pikachu es resistente al movimiento 'Flamethrower'?", resistsMove(pikachu, flamethrowerMove));
-  
-  function isWeakAgainst(attackData) {
-    const attackerType = attackData.attacker.type;
-    const attackedWeaknesses = attackData.attacked.modifiers.weakness;
-  
-    return attackedWeaknesses.includes(attackerType);
-  }
-  
-  const attackData = {
-    attacker: pikachu,
-    attacked: squirtle
-  };
-  
-  console.log("¿Squirtle es débil frente a Pikachu?", isWeakAgainst(attackData));
-  
-  function isStrongAgainst(attackData) {
-    const attackerType = attackData.attacker.type;
-    const attackedResistances = attackData.attacked.modifiers.resistances;
-  
-    return attackedResistances.includes(attackerType);
-  }
-  function addAbility(pokemon, abilityData) {
-    const abilityType = Object.keys(abilityData)[0];
-    const abilityName = abilityData[abilityType];
-  
-    if (abilityType === "primary" || abilityType === "hidden") {
-      if (!pokemon.ability[abilityType]) {
-        pokemon.ability[abilityType] = abilityName;
-      } else {
-        console.log(`El Pokémon ya tiene una habilidad ${abilityType}`);
-      }
+};
+
+function getMoves(pokemon) {
+  return pokemon.moves;
+}
+
+console.log("Movimientos de Pikachu:", getMoves(pikachu));
+
+function getPrimaryAbility(pokemon) {
+  return pokemon.ability?.primary || "Habilidad no disponible";
+}
+
+console.log("Habilidad primaria de Bulbasaur:", getPrimaryAbility(bulbasaur));
+
+function getWeaknesses(pokemon) {
+  return pokemon.modifiers.weakness;
+}
+
+console.log("Tipos contra los que es débil Charmander:", getWeaknesses(charmander));
+
+function resistsType(pokemon, type) {
+  const resistances = pokemon.modifiers.resistances;
+  return resistances.includes(type);
+}
+
+console.log("¿Charmander es resistente al tipo 'electric'?", resistsType(charmander, "electric"));
+console.log("¿Charmander es resistente al tipo 'fire'?", resistsType(charmander, "fire"));
+
+function resistsMove(pokemon, move) {
+  const resistances = pokemon.modifiers.resistances;
+  return resistances.includes(move.type);
+}
+
+const rainDanceMove = {
+  name: "Rain dance",
+  type: "water"
+};
+const flamethrowerMove = {
+  name: "Flamethrower",
+  type: "fire"
+};
+
+console.log("¿Pikachu es resistente al movimiento 'Rain dance'?", resistsMove(pikachu, rainDanceMove));
+console.log("¿Pikachu es resistente al movimiento 'Flamethrower'?", resistsMove(pikachu, flamethrowerMove));
+
+function isWeakAgainst(attacker, attacked) {
+  const attackerType = attacker.type;
+  const attackedWeaknesses = attacked.modifiers.weakness;
+
+  return attackedWeaknesses.includes(attackerType);
+}
+
+const attackData = {
+  attacker: pikachu,
+  attacked: squirtle
+};
+
+console.log("¿Squirtle es débil frente a Pikachu?", isWeakAgainst(attackData.attacker, attackData.attacked));
+
+function isStrongAgainst(attacker, attacked) {
+  const attackerType = attacker.type;
+  const attackedResistances = attacked.modifiers.resistances;
+
+  return attackedResistances.includes(attackerType);
+}
+
+function addAbility(pokemon, abilityData) {
+  const abilityType = Object.keys(abilityData)[0];
+  const abilityName = abilityData[abilityType];
+
+  if (abilityType === "primary" || abilityType === "hidden") {
+    if (!pokemon.ability[abilityType]) {
+      pokemon.ability[abilityType] = abilityName;
     } else {
-      console.log(`Tipo de habilidad no válido: ${abilityType}`);
+      console.log(`El Pokémon ya tiene una habilidad ${abilityType}`);
     }
-  
-    return pokemon;
+  } else {
+    console.log(`Tipo de habilidad no válido: ${abilityType}`);
   }
-  const abilityData = {
-    secondary: "Discharge"
-  };
-  
-  const pikachuWithAbility = addAbility(pikachu, abilityData);
-  console.log("Pikachu con nueva habilidad:", pikachuWithAbility);
-  
-  
+
+  return pokemon;
+}
+
+const abilityData = {
+  primary: "Discharge"
+};
+
+const pikachuWithAbility = addAbility(pikachu, abilityData);
+console.log("Pikachu con nueva habilidad:", pikachuWithAbility);
+
 function addMove(pokemon, move) {
-    return {
-      ...pokemon,
-      moves: [...pokemon.moves, move]
-    };
+  return {
+    ...pokemon,
+    moves: [...pokemon.moves, move]
+  };
+}
+
+const newMove = "Discharge";
+const pikachuWithMove = addMove(pikachu, newMove);
+console.log("Pikachu con nuevo movimiento:", pikachuWithMove);
+
+function removeMove(pokemon, moveToRemove) {
+  const updatedMoves = pokemon.moves.filter((move) => move !== moveToRemove);
+
+  return {
+    ...pokemon,
+    moves: updatedMoves
+  };
+}
+
+const moveToRemove = "Iron Tail";
+const pikachuWithoutMove = removeMove(pikachu, moveToRemove);
+
+console.log("Pikachu sin el movimiento 'Iron Tail':", pikachuWithoutMove);
+
+function getAttackModifier(attacker, attacked) {
+  const attackerType = attacker.type;
+  const attackedWeaknesses = attacked.modifiers.weakness;
+  const attackedResistances = attacked.modifiers.resistances;
+
+  if (attackedWeaknesses.includes(attackerType)) {
+    return 2;
+  } else if (attackedResistances.includes(attackerType)) {
+    return 0.5;
+  } else {
+    return 1;
   }
-  
-  const newMove = "Discharge";
-  const pikachuWithMove = addMove(pikachu, newMove);
-  console.log("Pikachu con nuevo movimiento:", pikachuWithMove);
+}
 
 
-  function removeMove(pokemon, moveToRemove) {
-    const updatedMoves = pokemon.moves.filter((move) => move !== moveToRemove);
-  
-    return {
-      ...pokemon,
-      moves: updatedMoves
-    };
+function getAttackLog(attacker, attacked, damage) {
+  const effectiveness = getAttackModifier(attacker, attacked);
+  const isSuperEffective = effectiveness > 1;
+  const move = attacker.moves[Math.floor(Math.random() * attacker.moves.length)];
+
+  let log = `${attacker.name} used ${move}! ${attacked.name} lost ${damage} HP!`;
+  if (isSuperEffective) {
+    log += " It's super effective!";
   }
-  
-  const moveToRemove = "Iron Tail";
-  const pikachuWithoutMove = removeMove(pikachu, moveToRemove);
-  
-  console.log("Pikachu sin el movimiento 'Iron Tail':", pikachuWithoutMove);
 
-  
-  function getAttackModifier(attackData) {
-    const attackerType = attackData.attacker.type;
-    const attackedWeaknesses = attackData.attacked.modifiers.weakness;
-    const attackedResistances = attackData.attacked.modifiers.resistances;
-  
-    if (attackedWeaknesses.includes(attackerType)) {
-      return 2;
-    } else if (attackedResistances.includes(attackerType)) {
-      return 0.5;
-    } else {
-      return 1;
-    }
-  }
-  const attackModifier = getAttackModifier(attackData);
-  console.log("Modificador de ataque:", attackModifier);
-
-
-function getAttackLog(attackData) {
-    const { attacker, attacked, move, damage } = attackData;
-    console.log(attacker);
-    console.log(attacked);
-    console.log(move);
-    console.log(damage);
-    return `${attacker} used ${move}! ${attacked} lost ${damage} HP!`;
+  return log;
 }
 
-  const attackLog = getAttackLog(attackData);
-  console.log(attackLog);
 
-  function calculateDamage(attacker, attacked) {
-    const attackPower = attacker.stats.attack;
-    const deffensePower = attacked.stats.deffense;
-    const modifiers = getAttackModifier({
-            attacker: attacker,
-            attacked: attacked,
-    });
-    return Math.floor((2 * attacker.level / 5 + 2) * attackPower / deffensePower);
+function calculateDamage(attacker, attacked) {
+  const attackerAttack = attacker.stats.attack;
+  const attackedDefense = attacked.stats.defense;
+  const modifier = getAttackModifier(attacker, attacked);
+
+  const damage = 0.5 * attackerAttack * (attackerAttack / attackedDefense) * modifier;
+  return damage;
 }
 
-function getRandomMove(pokemon) {
-    const moves = pokemon.moves
-    const randomIndex = Math.floor(Math.random() * moves.length)
-    return moves [randomIndex];
-}
-
-function bettle(pokemon1, pokemon2) {
-    const bettleResult ={
-        rounds: 0,
-        results: {
-            winner: null,
-            loser: null
-        },
-        history: []
-    };
-}
-
+const damageToSquirtle = calculateDamage(attackData.attacker, attackData.attacked);
+console.log("Daño ocasionado a Squirtle:", damageToSquirtle)
 
 function battle(pokemon1, pokemon2) {
+  let winner = null;
+  let loser = null;
   let rounds = 0;
   const history = [];
 
-  const attackingPokemon = { ...pokemon1 };
-  const attackedPokemon = { ...pokemon2 };
+  while (pokemon1.stats.hp > 0 && pokemon2.stats.hp > 0) {
+    const firstAttacker = pokemon1.stats.speed >= pokemon2.stats.speed ? pokemon1 : pokemon2;
+    const secondAttacker = firstAttacker === pokemon1 ? pokemon2 : pokemon1;
 
-  while (attackingPokemon.stats.hp > 0 && attackedPokemon.stats.hp > 0) {
-    const move = getRandomMove(attackingPokemon);
-    const damage = calculateDamage(attackingPokemon, attackedPokemon);
-    attackedPokemon.stats.hp = Math.max(attackedPokemon.stats.hp - damage, 0);
+    const damageToSecond = calculateDamage(firstAttacker, secondAttacker);
+    secondAttacker.stats.hp -= damageToSecond;
 
-    const attackLog = getAttackLog({
-      attacker: attackingPokemon.name,
-      attacked: attackedPokemon.name,
-      move: move,
-      damage: damage,
-      modifier: damage > calculateDamage(attackingPokemon, attackedPokemon) ? "weak" : "resistant"
-    });
+    const damageToFirst = calculateDamage(secondAttacker, firstAttacker);
+    firstAttacker.stats.hp -= damageToFirst;
 
-    history.push(attackLog);
+    const logFirst = getAttackLog(firstAttacker, secondAttacker, damageToSecond);
+    history.push(logFirst);
+
+    if (secondAttacker.stats.hp <= 0) {
+      winner = firstAttacker;
+      loser = secondAttacker;
+      break;
+    }
+
+    const logSecond = getAttackLog(secondAttacker, firstAttacker, damageToFirst);
+    history.push(logSecond);
+
+    if (firstAttacker.stats.hp <= 0) {
+      winner = secondAttacker;
+      loser = firstAttacker;
+      break;
+    }
+
     rounds++;
-    const temp = attackingPokemon;
-    attackingPokemon - attackedPokemon;
-    attackedPokemon - temp;
   }
 
-  const winner = pokemon1.stats.hp > 0 ? pokemon1 : pokemon2;
-  const loser = pokemon1.stats.hp > 0 ? pokemon2 : pokemon1;
+  const results = {
+    winner: {
+      name: winner.name,
+      hp: winner.stats.hp
+    },
+    loser: {
+      name: loser.name,
+      hp: loser.stats.hp
+    }
+  };
 
   return {
     rounds: rounds,
-    results: {
-      winner: {
-        name: winner.name,
-        hp: winner.stats.hp
-      },
-      loser: {
-        name: loser.name,
-        hp: loser.stats.hp
-      }
-    },
+    results: results,
     history: history
   };
 }
@@ -318,6 +310,3 @@ function battle(pokemon1, pokemon2) {
 const battleResult = battle(pikachu, squirtle);
 console.log(battleResult);
 
-  
-      
-      
